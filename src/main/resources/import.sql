1d2d61fae6ebf357b4bb2c990d52165b01abecd4
Jane Márcia <jamin2350@gmail.com>
2025-07-18 21:04:23 -0300

    Adiciona arquivos de configuração e script de importação SQL

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO materia (nome) VALUES ('Matemática');
+INSERT INTO materia (nome) VALUES ('História');
+INSERT INTO quiz (nome, descricao, materia_id) VALUES ('Quiz de Matemática', 'Desafios matemáticos', 1);
+INSERT INTO quiz (nome, descricao, materia_id) VALUES ('Quiz de História', 'Desafios históricos', 2);
