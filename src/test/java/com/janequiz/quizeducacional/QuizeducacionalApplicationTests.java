d1c5bfa002bb2a6b2568dee73820d5e83762e201
Jane Márcia <jamin2350@gmail.com>
2025-07-19 21:03:50 -0300

    test: teste padrão da aplicação Spring Boot

---
+++ src/test/java/com/janequiz/quizeducacional/QuizeducacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janequiz.quizeducacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizeducacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
